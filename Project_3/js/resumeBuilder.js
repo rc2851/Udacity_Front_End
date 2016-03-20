f6ca589d12a119db90a3101225cd38c96a251a0f
rc2851 <bobcannonjr@hotmail.com>
2016-03-20 17:12:02 -0500

    Project 3
    Modified resumeBuilder.js

--- Project_3/js/resumeBuilder.js
+++ Project_3/js/resumeBuilder.js
@@ -19,29 +19,17 @@ $("#header").append(formattedWelcomeMsg);
 
 var work = {
     "jobs": [{
-        "employer": "Udacity",
-        "title": "Course Developer",
-        "location": "Mountain View, CA",
-        "dates": "Feb 2014 - Current",
-        "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+        "employer": "AT&T",
+        "title": "Sr. Application Developer",
+        "location": "St Louis, MO",
+        "dates": "June 2000 - Current",
+        "description": "Application developer."
     }, {
-        "employer": "LearnBIG",
-        "title": "Software Engineer",
-        "location": "Seattle, WA",
-        "dates": "May 2013 - Jan 2014",
-        "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
-    }, {
-        "employer": "LEAD Academy Charter High School",
-        "title": "Science Teacher",
-        "location": "Nashville, TN",
-        "dates": "Jul 2012 - May 2013",
-        "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
-    }, {
-        "employer": "Stratford High School",
-        "title": "Science Teacher",
-        "location": "Nashville, TN",
-        "dates": "Jun 2009 - Jun 2012",
-        "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+        "employer": "Pillsbury",
+        "title": "Sanitation Supervisor",
+        "location": "St Louis, MO",
+        "dates": "1986 - June 2000",
+        "description": "Sanitation supervison."
     }]
 };
 
